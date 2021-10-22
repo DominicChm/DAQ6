cc2abcc616bff8d5cd17d4815384b15a66bf906f
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-10-22 08:56:46 -0700

    Update marker sensor
    Now only logs on state change

--- src/sensors/SensorMarker.h
+++ src/sensors/SensorMarker.h
@@ -4,19 +4,22 @@
 class SensorMarker : public Sensor {
 private:
     int pin;
+    bool last_state;
 public:
     SensorMarker(uint8_t id, int pin) : Sensor(id) {
         this->pin = pin;
         pinMode(this->pin, INPUT_PULLUP);
+        last_state = digitalRead(pin);
     };
 
     uint16_t readPacketBlock(uint8_t *buffer) override {
-        if (!digitalRead(pin)) {
+        bool state = digitalRead(pin);
+        if (last_state != state) {
             buffer[0] = id;
-            buffer[1] = 0x01;
+            buffer[1] = !state;
             sensorPrint("Marker;\t");
+            last_state = state;
             return 2;
-
         } else {
             return 0;
         }
